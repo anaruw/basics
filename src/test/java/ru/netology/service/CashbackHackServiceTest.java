8b96ec105ff6853412442f3df42e1bb58d806b94
Andrey Kononenko <anaruw_13@mail.ru>
2025-02-25 16:59:35 +0300

    junit-jupiter tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -48,6 +49,54 @@ public class CashbackHackServiceTest {
         int amount = 0;
 
         Assert.assertThrows(RuntimeException.class, ()->{
+
+            int actual = service.remain(amount);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterRemainWithAmountMultipleOfBoundary() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 4000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterRemainWithAmountNonMultipleOfBoundary() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 795;
+
+        int expected = 205;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterRemainWithNegativeAmount() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = -1;
+
+        Assertions.assertThrows(RuntimeException.class, ()->{
+
+            int actual = service.remain(amount);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterRemainWithZeroAmount() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        Assertions.assertThrows(RuntimeException.class, ()->{
 
             int actual = service.remain(amount);
         });
